a69673ebc47bfc01f164875278e124ddf7d17fd7
Mahmudjon <mahmudshokirow@gmail.com>
2021-01-10 21:13:22 +0500

    token10

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def start(update, context):
 
 def main():
     # Updaterni o'rnatib olamiz
-    updater = Updater('Token8', use_context=True)
+    updater = Updater('Token10', use_context=True)
 
     # Dispatcherni eventlarini aniqlash uchun
     dispatcher = updater.dispatcher
